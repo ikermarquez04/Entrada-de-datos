1487ff373760eb3769cdb9652fa97ca13d5ed919
ikerm <ikerm@LAPTOP-155AQCB8>
2023-01-09 20:28:30 -0600

    Agregar archivos de proyecto.

---
+++ Entrada de datos/Entrada de datos.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	float numero;
+
+	cout << "digite un numero: ";
+	cin >> numero;
+
+	cout << "\nTu numero es: " << numero;
+
+	return 0;
+}
